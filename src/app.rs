9434ab623db04967e37dd0df2a3111f9ffea7306
Dimitrii <dimitrii@pop-os.localdomain>
2023-03-23 15:41:50 +0300

    Added reset button && minor refactoring

--- src/app.rs
+++ src/app.rs
@@ -17,17 +17,35 @@ pub fn app() -> Html {
         let counter = counter.clone();
 
         Callback::from(move |_| {
-            let value = *counter - 1;
+            let value = {
+                if *counter > 0 {
+                    *counter - 1
+                } else {
+                    *counter
+                }
+            };
             counter.set(value);
         })
     };
 
+    let reset = {
+        let counter = counter.clone();
+        Callback::from(move |_| {
+            counter.set(0);
+        })
+    };
+
     html! {
-        <>
-            <button onclick={increase}>{ "+1" }</button>
-            <button onclick={decrease}>{ "-1" }</button>
-            <p>{ *counter }</p>
-        </>
+        <div class={classes!("counter")}>
+            <img src="img/WASM-logo.svg" alt="Logo of WebAssembly" width={ 200 }/>
+            <h1>{ "Yew Counter" }</h1>
+            <span class={classes!("counter__output")}>{*counter}</span>
+            <div class={classes!("btn__container")}>
+                <button class={classes!("control_btn")} onclick={increase}>{ "+1" }</button>
+                <button class={classes!("control_btn")} onclick={decrease}>{ "-1" }</button>
+                <button class={classes!("reset")} onclick={reset}>{ "Reset" }</button>
+            </div>
+        </div>
 
     }
 }
